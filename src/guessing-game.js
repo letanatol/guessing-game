0d8c37976de760121bc0bb9cfbfcbd019a47ada0
letanatol <letanatol@gmail.com>
2023-10-12 01:51:18 +0200

    feat: add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
+// const number = 409;
+// const game = new GuessingGame();
+// game.setRange(0, 4048)
+// let result = game.guess();
+// game.lower();
+
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
-    setRange(min, max) {
-
+    setRange(min, max) { //этот метод принимает минимальное и максимальное значение диапазона чисел для угадывания
+        this.min = min;
+        this.max = max;
     }
 
-    guess() {
-
+    guess() { // этот метод возвращает кандидата на решение (вы делаете предположение на основе диапазона и предыдущих предположений)
+        this.result = Math.round((this.min + this.max) / 2);
+        return this.result;
     }
 
-    lower() {
-
+    lower() { // этот метод вызывается, если предыдущий вызов guess()возвращаемого номера больше, чем ответ
+        this.max = this.result;
     }
 
-    greater() {
-
+    greater() { // этот метод вызывается, если предыдущий вызов guess()возвращаемого номера меньше, чем ответ
+        this.min = this.result;
     }
 }
 
